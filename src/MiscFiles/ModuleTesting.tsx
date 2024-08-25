4ca8d87d0f37e786a31e457ee114f1be2cedddd3
Lucas <lucasvanderwielen@icloud.com>
2024-08-25 15:30:47 -0400

    thing

--- src/MiscFiles/ModuleTesting.tsx
+++ src/MiscFiles/ModuleTesting.tsx
@@ -4,26 +4,22 @@ import ModuleView from "../ModuleView/ModuleView"
 
 function ModuleTesting() {
 
-    const number = 0
+    const number = 3
     const softReset = useState(false)
     const hardReset = useState(false)
     const isDone = useState(false)
     return (
         <>
+
             <ModuleView isDone={isDone} hardReset={hardReset} softReset={softReset} keyModuleNumber={0} displayModuleNumber={number}></ModuleView>
 
             <button onClick={() => { softReset[1]((value) => !value) }}>Soft Reset</button>
             <button onClick={() => { hardReset[1]((value) => !value) }}>Hard Reset</button>
 
             <div>{"Done: " + isDone[0]}</div>
+
         </>
     )
-
-    // return (
-    //     <>
-    //     <div>This is Working</div>
-    //     </>
-    // )
 }
 
 export default ModuleTesting
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.tsx'
+import App from './App.tsx'
 
-import './normalize.css'
+
 import './index.css'
+import './normalize.css'
 import ModuleTesting from './MiscFiles/ModuleTesting.tsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ModuleTesting></ModuleTesting>
+    <App></App>
   </React.StrictMode>,
 )
